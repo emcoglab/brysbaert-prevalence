2a132b77243c954bfdec8b6c211aca0a58ef1c45
Cai <caiw@users.noreply.github.com>
2022-05-26 15:17:00 +0100

    Running this file will cause it to download only

--- brysbaert_prevalence.py
+++ brysbaert_prevalence.py
@@ -1,4 +1,4 @@
-from logging import getLogger
+from logging import getLogger, basicConfig, INFO
 from pathlib import Path
 from typing import Set
 
@@ -16,24 +16,26 @@ class BrysbaertPrevalence:
     _file_path: Path = Path(Path(__file__).parent, "English_Word_Prevalences.xlsx")
     _file_url: str = "https://osf.io/nbu9e/download"
 
-    def _load_from_local_file(self) -> DataFrame:
-        with self._file_path.open("rb") as f:
+    @classmethod
+    def _load_from_local_file(cls) -> DataFrame:
+        with cls._file_path.open("rb") as f:
             return read_excel(f, sheet_name="Prevalence", header=0, index_col=None)
 
-    def _download_file(self) -> None:
+    @classmethod
+    def download_data_file(cls) -> None:
         """Downloads the data file from the internet and saves it in the expected location."""
-        _logger.info(f"Attempting to download data file from {self._file_url}")
-        excel_data: requests.Response = requests.get(self._file_url, allow_redirects=True)
-        if self._file_path.exists():
-            raise FileExistsError(self._file_path)
-        with self._file_path.open("wb") as f:
+        _logger.info(f"Attempting to download data file from {cls._file_url}")
+        excel_data: requests.Response = requests.get(cls._file_url, allow_redirects=True)
+        if cls._file_path.exists():
+            raise FileExistsError(cls._file_path)
+        with cls._file_path.open("wb") as f:
             f.write(excel_data.content)
 
     def __init__(self):
         if not self._file_path.exists():
             _logger.info("Data file doesn't exist")
             try:
-                self._download_file()
+                self.download_data_file()
             except Exception as e:
                 _logger.warning(f"File download failed: {e}")
         self.data: DataFrame = self._load_from_local_file()
@@ -52,6 +54,11 @@ class BrysbaertPrevalence:
 
 
 if __name__ == '__main__':
-    b = BrysbaertPrevalence()
-    p = b.prevalence_for("abbey")
-    print(p)
+    basicConfig(
+        format='%(asctime)s | %(levelname)s | %(funcName)s @ %(module)s:%(lineno)d |➤ %(message)s',
+        datefmt="%Y-%m-%d %H:%M:%S",
+        level=INFO)
+    try:
+        BrysbaertPrevalence.download_data_file()
+    except Exception as e:
+        logger.warning(f"Download failed: {e}")
