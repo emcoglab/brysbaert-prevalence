d714270a5e031e03f9ceb40fa6425082cd2a85e1
Cai Wingfield <c.wingfield@lancaster.ac.uk>
2022-05-26 14:41:05 +0100

    Download data file if not included

--- brysbaert_prevalence.py
+++ brysbaert_prevalence.py
@@ -1,7 +1,11 @@
+from logging import getLogger
 from pathlib import Path
 from typing import Set
 
+import requests as requests
 from pandas import read_excel, DataFrame
+
+_logger = getLogger(__name__)
 
 
 class WordNotFoundError(LookupError):
@@ -10,10 +14,30 @@ class WordNotFoundError(LookupError):
 
 class BrysbaertPrevalence:
     _file_path: Path = Path(Path(__file__).parent, "English_Word_Prevalences.xlsx")
+    _file_url: str = "https://osf.io/nbu9e/download"
+
+    def _load_from_local_file(self) -> DataFrame:
+        with self._file_path.open("rb") as f:
+            return read_excel(f, sheet_name="Prevalence", header=0, index_col=None)
+
+    def _download_file(self) -> None:
+        """Downloads the data file from the internet and saves it in the expected location."""
+        _logger.info(f"Attempting to download data file from {self._file_url}")
+        excel_data: requests.Response = requests.get(self._file_url, allow_redirects=True)
+        if self._file_path.exists():
+            raise FileExistsError(self._file_path)
+        with self._file_path.open("wb") as f:
+            f.write(excel_data.content)
 
     def __init__(self):
-        with self._file_path.open("rb") as f:
-            self.data: DataFrame = read_excel(f, sheet_name="Prevalence", header=0, index_col=None)
+        if not self._file_path.exists():
+            _logger.info("Data file doesn't exist")
+            try:
+                self._download_file()
+            except Exception as e:
+                _logger.warning(f"File download failed: {e}")
+        self.data: DataFrame = self._load_from_local_file()
+
         self.data["Word"] = self.data["Word"].str.lower()
         self.words: Set[str] = set(
             tup.Word
